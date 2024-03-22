ef9ef3df210cd696382865982800f820393aa579
sathishkumar.s <sathishkumar.s@DESKTOP-2F032>
2024-03-22 12:11:28 +0530

    Employer Class Only Created

---
+++ Models/Employer.cs
@@ -0,0 +1,9 @@
+﻿namespace SathishShop.Models
+{
+    public class Employer
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpDepartment { get; set; }
+    }
+}
